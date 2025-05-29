8107cc81b4e5f10e09064d6f910aa81a005d3604
Leezed525 <leezed525@qq.com>
2025-05-29 17:36:58 +0800

    多补充了几个函数的图像

--- tmp.py
+++ tmp.py
@@ -1,4 +1,17 @@
-a = 5
+import matplotlib as plt
+import numpy as np
 
-a = [int(x) for x in bin(int(a))[2:]]
+
+
+
+
+if __name__ == '__main__':
+    dp1 = [0, 1, 1, 2, 4, 6, 9]
+    dp2 = [0, 1, 1, 2, 4, 6, 9]
+
+
+    for i in range(7,10000):
+        dp1.append(max(dp1[i - 2] * 2, dp1[i - 3] * 3))
+        dp2.append(dp2[i - 3] * 3)
+        print(i, dp1[i]==dp2[i])
 
