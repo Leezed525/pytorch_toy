3fe010b29201bf50fe78a1b16a15c91b04ddd46f
Leezed525 <leezed525@qq.com>
2025-07-01 00:17:12 +0800

    开始向量数据库的工作

---
+++ TinyRAG/VectorBase/LocalVectorBase.py
@@ -0,0 +1,5 @@
+"""
+-*- coding: UTF-8 -*-
+@Author  ：Leezed
+@Date    ：2025/6/30 23:23 
+"""
---
+++ TinyRAG/VectorBase/VectorBase.py
@@ -0,0 +1,29 @@
+"""
+-*- coding: UTF-8 -*-
+@Author  ：Leezed
+@Date    ：2025/6/30 23:21 
+"""
+
+from typing import List
+from TinyRAG.Embedding.BaseEmbedding import BaseEmbedding
+
+
+class VectorStore:
+    def __init__(self, document: List[str] = ['']) -> None:
+        self.document = document
+
+    def get_vector(self, EmbeddingModel: BaseEmbedding) -> List[List[float]]:
+        # 获得文档的向量表示
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def persist(self, path: str = 'storage'):
+        # 数据库持久化，本地保存
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def load_vector(self, path: str = 'storage'):
+        # 从本地加载数据库
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def query(self, query: str, EmbeddingModel: BaseEmbedding, k: int = 1) -> List[str]:
+        # 根据问题检索相关的文档片段
+        raise NotImplementedError("This method should be overridden by subclasses.")
